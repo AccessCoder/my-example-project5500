ab66ff1468580824f51761a32b31d5a99f0bb279
AccessCoder <info.unaddict@gmail.com>
2026-06-29 10:36:29 +0200

    add bullshit for sonar

---
+++ backend/src/main/java/org/example/backend/dupliclass.java
@@ -0,0 +1,22 @@
+package org.example.backend;
+
+public class dupliclass {
+
+    static void main() {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+
+
+        int i =1;
+
+
+        if ((i=4) == 5){
+            System.out.println("Hello World");
+        }
+    }
+}
